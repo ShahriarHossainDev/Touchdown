aa6519db0fb3c00b25ef48e90954e9dc0292e55c
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-07-19 14:42:28 +0600

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Shishir_Mac on 19/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
